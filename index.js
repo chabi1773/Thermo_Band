7edfcd3d0a78a2e3e84fa4adbb7058111fbf36c4
NileshAm <nileshamarathunge@gmail.com>
2025-06-30 21:34:48 +0530

    jwt made to work

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const express = require('express');
-const cors = require('cors');
-
-const patientsRouter = require('./routes/patients');
-const temperaturesRouter = require('./routes/temperatures');
-const devicePatientRouter = require('./routes/devicepatient');
-const esp32Router = require('./routes/esp32');
+const patientsRouter = require("./routes/patients");
+const temperaturesRouter = require("./routes/temperatures");
+const devicePatientRouter = require("./routes/devicepatient");
+const esp32Router = require("./routes/esp32");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/patients',patientsRouter);
-app.use('/temperatures',temperaturesRouter);
-app.use('/devicepatient', devicePatientRouter);
-app.use('/esp32', esp32Router); // change verifyApiKey to esp32Router
-
-
-app.get('/', (req, res) => {
-  res.send('Health Monitoring Backend API is running');
-});
-
-app.use((req, res) => {
-  res.status(404).json({ error: 'Not found' });
+app.get("/", (req, res) => {
+  res.send("Health Monitoring Backend API is running");
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
+  res.status(500).json({ error: "Something went wrong!" });
+});
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // 'Bearer <token>'
+
+  if (!token) return res.status(401).json({ error: "No token provided" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.SUPABASE_JWT_SECRET); // This secret is from Supabase settings
+    req.user = decoded;
+    next();
+  } catch (err) {
+    console.log(err)
+    return res.status(403).json({ error: "Invalid token" });
+  }
+};
+app.use("/patients", authenticateToken, patientsRouter);
+app.use("/temperatures", authenticateToken, temperaturesRouter);
+app.use("/devicepatient", authenticateToken, devicePatientRouter);
+app.use("/esp32", esp32Router); // change verifyApiKey to esp32Router
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Not found" });
 });
 
 const PORT = process.env.PORT || 5000;
