83b1ecef06fd5e87fadbd419b1e7576c91ce0e52
Chamod_Sandeepa <144690578+chabi1773@users.noreply.github.com>
2025-07-01 12:24:57 +0530

    Undo ipv4

--- index.js
+++ index.js
@@ -59,4 +59,4 @@ app.use((err, req, res, next) => {
 
 // ✅ Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0" , () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT , () => console.log(`Server running on port ${PORT}`));
